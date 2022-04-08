cdb4101aa6466c3fa99714d7092ac8474a77d827
StanislavGarber <stasbvk@gmail.com>
2022-04-08 23:12:38 +0300

    initial

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <map>
+
+using std::cout; using std::cerr; using std::endl; using std::string;
+using std::ifstream; using std::ostringstream; using std::istringstream;
+
+string read_file_into_string(const string& path) {
+    auto ss = ostringstream{};
+}
+
+int main()
+{
+
+}
